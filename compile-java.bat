8e70264d09a987c4cea772bbd884ca2f48a0c467
Pavel Petrovic <pavel.petrovic@gmail.com>
2019-06-28 09:51:15 +0200

    adding pritnout of java version

--- compile-java.bat
+++ compile-java.bat
@@ -1,5 +1,6 @@
-SET PATH="C:\Program Files\Java\jdk1.8.0_211\bin\";%PATH%
+SET PATH="C:\Program Files\Java\jdk1.8.0_121\bin\";%PATH%
 cd src\java
+javac -version
 javac *.java
 jar cvfm ..\..\lamp.jar META-INF\MANIFEST.MF *.class
 del *.class
